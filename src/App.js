618d8b3d680e46b1de185f1929f85e7c0a4ce666
haley-r <haleyryan77@gmail.com>
2020-04-11 12:21:05 -0500

    added marked.js, changes md input to html output in string form. next, figure out how to style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>haley's markdown editor</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    mdInput:'',
+    htmlOutput: ''
+  }
+
+  showHTML=()=>{
+    // Create reference instance
+    const marked = require('marked');
+
+    // Set options
+    // `highlight` example uses `highlight.js`
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      // highlight: function (code, language) {
+      //   const hljs = require('highlight.js');
+      //   const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+      //   return hljs.highlight(validLanguage, code).value;
+      // },
+      pedantic: false,
+      gfm: true,
+      breaks: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false
+    });
+
+    // Compile
+    this.setState({htmlOutput: (marked(this.state.mdInput))});
+  }
+
+  updateInput=(event)=>{
+    this.setState({mdInput: event.target.value})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>haley's markdown editor</h1>
+        </header>
+        <div id="input-area">
+          <label htmlFor="md-input">type markdown here:</label>
+            <textarea onChange={this.updateInput}/>
+          <button onClick={this.showHTML}>show html</button>
+        </div>
+        <div id="output-area">
+            {this.state.htmlOutput}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
